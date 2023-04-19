9015ddc0f9f4a82c9850d7557b6e60ea4ad5250d
Cristian Jorquera <130778123+CjRobx@users.noreply.github.com>
2023-04-19 11:13:36 -0400

    subida de archivos
    agregando archivos para proyecto

---
+++ js/scrypt.js
@@ -0,0 +1,39 @@
+var num1,num2,res;
+
+function realizarSuma(){
+    num1=parseInt.getElementbyId();
+    num2=parseInt.getElementbyId();
+    res=num1+num2
+    alert(res);
+}
+
+
+function realizarResta(){
+    num1=parseInt.getElementbyId();
+    num2=parseInt.getElementbyId();
+    res=num1-num2
+
+}
+
+function realizarMulti(){
+    num1=parseInt.getElementbyId();
+    num2=parseInt.getElementbyId();
+    res=num1*num2
+    alert(res);
+}
+
+function realizarDivi(){
+
+    if(num2===0){
+        alert("Debe ser un numero distinto de 0");
+    }
+
+}
+
+function resultado()
+{
+
+
+    
+}
+
